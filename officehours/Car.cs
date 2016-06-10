9c1f66db90d8dfa468c7d49c7aef2714057f72d5
Joe Johnson <joe.wendell.johnson@gmail.com>
2016-06-10 10:26:25 -0500

    Add project files.

---
+++ officehours/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adamsCar
+{
+    class Car
+    {
+
+        public decimal Value { get; private set; }
+
+        int Mileage;
+
+        public Car(string color = "yellow", decimal value = 0, int mileage = 0)
+        {
+            Color = color;
+            Value = value;
+            Mileage = mileage;
+        }
+
+       
+        public void Paint(string color)
+        {
+            Color = color;
+            if(color == "red")
+            {
+                Value = Value * 1.5m;
+            }
+            else
+            {
+                Value = Value * 0.9m;
+            }
+        }
+
+        public string Color { get; private set;}
+
+        public void Drive(int miles)
+        {
+            Mileage += miles;
+            if (miles > 100)
+            {
+                Value = Value * 0.99m;
+            }
+            //Do some code in here to drive the car
+        }
+    }
+}
---
+++ officehours/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adamsCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car adamsCar = new Car("red", 5000, 10000);
+            adamsCar.Drive(100);
+            Console.WriteLine("Adam's car is now worth: {0}", adamsCar.Value);
+            adamsCar.Drive(5000);
+            Console.WriteLine("Adam's car is now worth: {0}", adamsCar.Value);
+
+            adamsCar.Paint("beige");
+            Console.WriteLine("Adam's car is now worth: {0}", adamsCar.Value);
+
+            adamsCar.Drive(5000);
+            Console.WriteLine("Adam's car is now worth: {0}", adamsCar.Value);
+
+            adamsCar.Paint("red");
+            Console.WriteLine("Adam's car is now worth: {0}", adamsCar.Value);
+
+            string currentColor = adamsCar.Color;
+
+        }
+    }
+}
